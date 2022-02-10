c60427082a2a2723ae201a9c1396f7afcb57ebb1
b41-41 <41@b41.kr>
2022-02-11 00:33:57 +0900

    Modify - App.tsx 원상 복구
    - 지난 커밋에서 App.tsx 파일을 수정한 채로 커밋
    - App.tsx 파일 원상 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import DelReplyBtn from 'components/DelReplyBtn/DelReplyBtn';
 
 const AppLayout = styled.div`
   height: 100vh;
@@ -13,15 +12,13 @@ const PhoneLayout = styled.main`
   max-height: 920px;
   width: 100%;
   height: 100%;
-  border: 1px solid ${props => props.theme.colors.chatBlack};
+  border: 1px solid ${props => props.theme.colors.black};
   border-radius: 10px;
 `;
 
 const App = () => (
   <AppLayout>
-    <PhoneLayout>
-      <DelReplyBtn />
-    </PhoneLayout>
+    <PhoneLayout></PhoneLayout>
   </AppLayout>
 );
 export default App;
