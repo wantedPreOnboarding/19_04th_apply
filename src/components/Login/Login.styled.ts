bddd1088ad438b959d4d97abb7b793f1d0c607a1
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-12 02:05:09 +0900

    Add Login 스타일링

--- src/components/Login/Login.styled.ts
+++ src/components/Login/Login.styled.ts
@@ -2,34 +2,94 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   position: absolute;
-  bottom: 0;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
+  padding: 10px;
+  color: ${props => props.theme.colors.chatBlack};
+  background: ${props => props.theme.colors.chatPink};
+  border-radius: ${props => props.theme.borderRadius.normal};
 `;
 
-export const imageInput = styled.section`
+export const Inner = styled.div`
   height: 100%;
+  width: 100%;
   display: flex;
+  box-sizing: border-box;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: sirGray;
-  label {
-    display: block;
-    width: 200px;
-    padding: 10px 0;
-    border: 2px solid sirDPurple;
-    color: sirDgray;
-    font-weight: 600;
-    text-align: center;
-    cursor: pointer;
-    border-radius: 5px;
+  background: ${props => props.theme.colors.chatWhite};
+`;
+
+export const ImageInputBox = styled.section`
+  margin-top: 10px;
+`;
+
+export const ImageLabel = styled.label`
+  display: block;
+  width: 200px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${props => props.theme.colors.chatLGray};
+  font-weight: 600;
+  text-align: center;
+  cursor: pointer;
+  border-radius: ${props => props.theme.borderRadius.normal};
+  font-size: ${props => props.theme.fontSize.xl};
+  font-weight: ${props => props.theme.fontWeight.semiStrong};
+  &:hover {
+    filter: brightness(90%);
   }
-  input[type='file'] {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    clip: rect(0, 0, 0, 0);
-    overflow: hidden;
+`;
+
+export const ImageInput = styled.input.attrs({ type: 'file' })`
+  display: none;
+`;
+export const Avatar = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: ${props => props.theme.borderRadius.normal};
+`;
+
+export const NameInput = styled.input`
+  outline: none;
+  border: none;
+  text-align: center;
+  width: 95%;
+  font-size: ${props => props.theme.fontSize.xl};
+  font-weight: ${props => props.theme.fontWeight.semiStrong};
+  background: none;
+`;
+
+export const NameInputBox = styled.section`
+  display: block;
+  width: 200px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 5px solid ${props => props.theme.colors.chatLGray};
+  font-weight: 600;
+  text-align: center;
+  border-radius: ${props => props.theme.borderRadius.normal};
+  margin: 10px auto;
+  &:focus-within {
+    border: 5px solid ${props => props.theme.colors.chatPink};
+  }
+`;
+
+export const LoginBtn = styled.button`
+  background: ${props => props.theme.colors.chatTomato};
+  border-radius: ${props => props.theme.borderRadius.normal};
+  width: 200px;
+  height: 50px;
+  font-size: ${props => props.theme.fontSize.xl};
+  font-weight: ${props => props.theme.fontWeight.semiStrong};
+  &:hover {
+    filter: brightness(90%);
   }
 `;
