0b7680af4d5262d3190077cbce62e0e0af98b8ab
yeongjong <tohero21@naver.com>
2022-02-11 21:35:21 +0900

    Add - Portal을 만들기 위해 html 마크업 수정 및 globalStyle적용
    - root에 적용되는 기존 임시레이아웃 스타일드 컴포넌트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,6 @@
 import React from 'react';
-import styled from 'styled-components/macro';
-
-const AppLayout = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const PhoneLayout = styled.div`
-  max-width: 430px;
-  max-height: 920px;
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: rgb(0 0 0 / 30%) 0px 12px 60px 5px;
-`;
 
 const App = () => {
-  return (
-    <AppLayout>
-      <PhoneLayout></PhoneLayout>
-    </AppLayout>
-  );
+  return <div></div>;
 };
 export default App;
