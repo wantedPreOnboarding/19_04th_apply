c7a450c49b30a7965901b8f5820a5777772c9032
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-11 16:53:48 +0900

    Modify utils 이름 가시성을 위해 변경 Add  각 utils 테스트 코드 작성

--- src/utils/isEmpty.test.ts
+++
@@ -1,9 +0,0 @@
-import isEmpty from './isEmpty';
-
-test('공백을체크합니다.', () => {
-  expect(isEmpty('       ')).toBe(false);
-});
-
-test('공백을체크합니다.', () => {
-  expect(isEmpty('')).toBe(true);
-});
--- src/utils/startEndWhiteSpceRemove.ts
+++
@@ -1,5 +0,0 @@
-const startEndWhiteSpcaeRemove = (str: string) => {
-  return str.replace(/^\s+|\s+$/gm, '');
-};
-
-export default startEndWhiteSpcaeRemove;
