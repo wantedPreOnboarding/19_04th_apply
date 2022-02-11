a85f1c1f12a16097742b859cd46e33f6352a01dc
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-11 12:07:29 +0900

    Add 공통 utils 함수 분리

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { default as enterSubmitCheck } from './enterSubmitCheck';
+export { default as generateNextId } from './generateNextId';
+export { default as isEmpty } from './isEmpty';
+export { default as textareaHeightHandler } from './textareaHeightHandler';
