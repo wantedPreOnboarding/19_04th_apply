e12bfcffc46661c004c05132c259d477144c3e42
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-12 10:43:19 +0900

    Add Login 스타일링

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import * as S from './Login.styled';
 import { useAppSelector, useAppDispatch } from 'hooks/useStore';
 import { login } from 'store/slices/auth';
+
 const Login = () => {
   const dispatch = useAppDispatch();
   const [imageSrc, setImageSrc] = useState<string>('');
-  const avatarUrl = useAppSelector(state => state.auth.avatarURL);
-
   const readImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       setImageSrc(URL.createObjectURL(event.target.files[0]));
@@ -18,9 +17,31 @@ const Login = () => {
 
   return (
     <S.Wrapper>
-      <S.Inner>
-        {<S.Avatar alt="imageAvatar" id="imageAvatar" src={imageSrc ? imageSrc : avatarUrl} />}
-        <S.ImageInputBox>
+      <S.Logo>
+        <img
+          alt="imageAvatar"
+          id="imageAvatar"
+          src={`${process.env.REACT_APP_ASSET_PATH}/logoIcon.svg`}
+        />
+
+        <span>sweet</span>
+      </S.Logo>
+      <S.Header>
+        <span>Hello 👋 Sweeter!</span>
+      </S.Header>{' '}
+      <S.HeadMenu>
+        {imageSrc ? (
+          <S.Avatar alt="imageAvatar" id="imageAvatar" src={imageSrc} />
+        ) : (
+          <S.ChatIcon
+            alt="imageAvatar"
+            id="imageAvatar"
+            src={`${process.env.REACT_APP_ASSET_PATH}/chatIcon.svg`}
+          />
+        )}
+      </S.HeadMenu>
+      <S.Menu>
+        <S.MenuBox>
           <S.ImageLabel htmlFor="imageInput">프로필사진 등록</S.ImageLabel>
           <S.ImageInput
             id="imageInput"
@@ -31,12 +52,15 @@ const Login = () => {
               readImage(event);
             }}
           />
-        </S.ImageInputBox>
-        <S.NameInputBox>
+        </S.MenuBox>
+        <S.MenuBox>
           <S.NameInput id="nameInput" placeholder="이름을 입력해주세요"></S.NameInput>
-        </S.NameInputBox>
-        <S.LoginBtn>Login</S.LoginBtn>
-      </S.Inner>
+        </S.MenuBox>
+        <S.MenuBox>
+          <S.LoginBtn>Login</S.LoginBtn>
+        </S.MenuBox>
+      </S.Menu>
+      <S.Footer>s w e e t</S.Footer>
     </S.Wrapper>
   );
 };
