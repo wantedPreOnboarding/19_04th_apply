d2ee2e6203a6be93e2b5fcef3fa385e5e17b1fad
b41-41 <41@b41.kr>
2022-02-12 11:30:16 +0900

    Add - Portal을 이용해서 Modal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Header } from 'components';
+import DelModal from 'components/DelModal/DelModal';
+import Portal from 'components/Portal/Portal';
 import React from 'react';
 import Router from 'router/Router';
 
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <Header />
+      <Router />
+      <Portal>
+        <DelModal />
+      </Portal>
+    </>
+  );
 };
 export default App;
